9fe4513e90c3edd51170fad93e59848ee6f14c47
Diefesson <diefesson.so@gmail.com>
2021-02-15 13:45:14 -0300

    feat(backend): Added CORS support

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 import enviroment
 from web.sample_endpoint import sample_endpoint
 
 app: Flask = Flask(__name__)
 app.register_blueprint(sample_endpoint)
+
+CORS(app)
 
 
 @app.route('/')
